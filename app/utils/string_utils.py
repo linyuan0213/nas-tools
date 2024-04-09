4bdf8eaa1eb7f4433880d1c372fb571e06fa422c
C.C <linyuan213@gmail.com>
2024-04-09 09:42:08 +0800

    fix: bugfix

--- app/utils/string_utils.py
+++ app/utils/string_utils.py
@@ -558,6 +558,8 @@ class StringUtils:
         # 解析查询参数
         params = parse.parse_qs(parsed_url.query)
 
+        if not params.get('credential'):
+            return None
         credential = params.get('credential')[0]
         sign_data = base64.b64decode(credential).decode('utf-8')
         sign_params = parse.parse_qs(sign_data)
