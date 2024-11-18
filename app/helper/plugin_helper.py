5f30963284fa18f836bb8d5a4af43e947a097787
C.C <linyuan213@gmail.com>
2024-11-18 14:16:16 +0800

    fix: 移除无用组件

--- app/helper/plugin_helper.py
+++ app/helper/plugin_helper.py
@@ -10,17 +10,14 @@ class PluginHelper:
         """
         插件安装统计计数
         """
-        return RequestUtils(timeout=5).get(f"https://nastool.cn/plugin/{plugin_id}/install")
+        return None
 
     @staticmethod
     def report(plugins):
         """
         批量上报插件安装统计数据
         """
-        return RequestUtils(content_type="application/json",
-                            timeout=5).post(f"https://nastool.cn/plugin/update", json={
-            "plugins": [{"plugin_id": plugin, "count": 1} for plugin in plugins]
-        })
+        return None
 
     @staticmethod
     @cached(cache=TTLCache(maxsize=1, ttl=3600))
@@ -28,11 +25,4 @@ class PluginHelper:
         """
         获取插件安装统计数据
         """
-        ret = RequestUtils(accept_type="application/json",
-                           timeout=5).get_res("https://nastool.cn/plugin/statistic")
-        if ret:
-            try:
-                return ret.json()
-            except Exception as e:
-                print(e)
         return {}
